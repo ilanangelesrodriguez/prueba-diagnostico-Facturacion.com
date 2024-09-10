758f4d4c8b6febc502f650f421d1165e1b554ef6
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2024-09-10 22:03:33 +0000

    chore: eliminar tabla de productos y agregar ejecución de script SQL en la conexión a la base de datos

--- src/infrastructure/db/conexion.php
+++ src/infrastructure/db/conexion.php
@@ -11,10 +11,25 @@ class Conexion {
             try {
                 self::$conexion = new PDO($dsn, $username, $password);
                 self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                self::ejecutarScriptSQL('./script.sql');
             } catch (PDOException $e) {
                 throw new Exception("Error al conectar a la base de datos: " . $e->getMessage());
             }
         }
         return self::$conexion;
+    }
+
+    private static function ejecutarScriptSQL($archivo) {
+        if (file_exists($archivo)) {
+            $script = file_get_contents($archivo);
+            try {
+                self::$conexion->exec($script);
+            } catch (PDOException $e) {
+                throw new Exception("Error al ejecutar el script SQL: " . $e->getMessage());
+            }
+        } else {
+            throw new Exception("El archivo de script SQL no existe: " . $archivo);
+        }
     }
 }
