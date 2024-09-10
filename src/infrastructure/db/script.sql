f08650f2305bf3b8405a333d5b8611935eb2082c
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2024-09-10 22:55:36 +0000

    Refactorizar tabla de productos en la base de datos

--- src/infrastructure/db/script.sql
+++ src/infrastructure/db/script.sql
@@ -1,11 +1,10 @@
-CREATE TABLE productos (
-    id SERIAL PRIMARY KEY,
-    codigo VARCHAR(15) UNIQUE NOT NULL,
-    nombre VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS productos (
+    codigo VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
     bodega VARCHAR(100) NOT NULL,
     sucursal VARCHAR(100) NOT NULL,
     moneda VARCHAR(10) NOT NULL,
-    precio DECIMAL(10,2) NOT NULL,
-    materiales TEXT NOT NULL,
-    descripcion TEXT NOT NULL
+    precio DECIMAL(10, 2) NOT NULL,
+    materiales TEXT,
+    descripcion TEXT
 );
